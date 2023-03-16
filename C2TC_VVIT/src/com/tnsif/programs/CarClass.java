9610bc87974071e59eb2b386d5fbf41eafd10f6a
mohammed izhaar <mohammed izhaar@LAPTOP-PNAVTCPA>
2023-03-16 16:32:32 +0530

    Oops class 1

---
+++ C2TC_VVIT/src/com/tnsif/programs/CarClass.java
@@ -0,0 +1,18 @@
+package com.tnsif.programs;
+/*program to demonstrate class and object*/
+public class CarClass {
+	//all data members should be in lower case
+	int model;
+	String color;
+	int reg;
+	void forward() {
+		System.out.println("car is moving forward");
+	}
+	void backward() {
+		System.out.println("car is moving backward");
+	}
+	void stop() {
+		System.out.println("car has stopped");
+	}
+	
+}
---
+++ C2TC_VVIT/src/com/tnsif/programs/Executor.java
@@ -0,0 +1,12 @@
+package com.tnsif.programs;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		CarClass ob =new CarClass();
+		ob.forward();
+		ob.backward();
+		ob.stop();
+	}
+
+}
