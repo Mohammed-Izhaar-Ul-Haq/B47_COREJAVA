8377eb74da9e0e86d18bdb8f522f906b847c21de
mohammed izhaar <mohammed izhaar@LAPTOP-PNAVTCPA>
2023-04-18 15:07:15 +0530

    sync assignment

---
+++ C2TC_VVIT/src/com/tnsif/generics/GenericExample.java
@@ -0,0 +1,10 @@
+package com.tnsif.generics;
+
+public class GenericExample <T> {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ C2TC_VVIT/src/com/tnsif/syncassignment/Demo.java
@@ -0,0 +1,32 @@
+package com.tnsif.syncassignment;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		TrainTicketBooking booking = new TrainTicketBooking(10);
+
+        Thread t1 = new Thread(new Runnable() {
+            public void run() {
+                booking.bookTicket(5);
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            public void run() {
+                booking.bookTicket(1);
+            }
+        });
+
+        Thread t3 = new Thread(new Runnable() {
+            public void run() {
+                booking.bookTicket(4);
+            }
+        });
+
+        t1.start();
+        t2.start();
+        t3.start();
+
+	}
+
+}
